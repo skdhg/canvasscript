401b223c2d0d87ebddbce536fc0526b2a99e16f3
skdhg <46562212+skdhg@users.noreply.github.com>
2022-11-24 15:02:44 +0545

    some stuff

--- dev/index.js
+++ dev/index.js
@@ -3,10 +3,11 @@ const { createCanvas } = require("../lib/Canvas");
 const canvas = createCanvas(100, 100);
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#7289da";
+ctx.fillStyle = "#f2a5aa";
 ctx.fillRect(0, 0, 100, 100);
 
-ctx.strokeStyle = "#f8e354";
+ctx.strokeStyle = "#ff4444";
 ctx.strokeRect(0, 0, 100, 100);
 
-require("fs").writeFileSync("./hello.png", canvas.encodeSync("png"))
+
+require("fs").writeFileSync(`${__dirname}/hello.png`, canvas.encodeSync("png"))
--- src/context.rs
+++ src/context.rs
@@ -1,33 +1,45 @@
-use raqote::{DrawOptions, DrawTarget, SolidSource, Source, PathBuilder, StrokeStyle, LineCap, LineJoin};
+use raqote::{
+  DrawOptions, DrawTarget, PathBuilder, SolidSource, Source, StrokeStyle, LineJoin, LineCap,
+};
+use napi::{Result, bindgen_prelude::Buffer};
 
 #[path = "./utils.rs"]
 mod utils;
 
-#[napi(js_name = "DrawContext")]
-pub struct DrawContext {
+#[napi]
+pub struct CanvasRenderingContext2D {
   engine: DrawTarget,
   canvas_fill_style: String,
   canvas_stroke_style: String,
+  canvas_stroke_config: StrokeStyle,
 }
 
 #[napi]
-impl DrawContext {
+impl CanvasRenderingContext2D {
   #[napi(constructor)]
   pub fn new(width: i32, height: i32) -> Self {
-    DrawContext {
+    CanvasRenderingContext2D {
       engine: DrawTarget::new(width, height),
       canvas_fill_style: "#000000".to_string(),
       canvas_stroke_style: "#000000".to_string(),
+      canvas_stroke_config: StrokeStyle {
+        width: 1.0,
+        miter_limit: 10.0,
+        join: LineJoin::Miter,
+        cap: LineCap::Butt,
+        dash_array: vec![],
+        dash_offset: 0.0
+      },
     }
   }
 
   #[napi(getter)]
-  pub fn width(&self) -> napi::Result<i32> {
+  pub fn width(&self) -> Result<i32> {
     Ok(self.engine.width())
   }
 
   #[napi(getter)]
-  pub fn height(&self) -> napi::Result<i32> {
+  pub fn height(&self) -> Result<i32> {
     Ok(self.engine.height())
   }
 
@@ -41,7 +53,7 @@ impl DrawContext {
   }
 
   #[napi(getter)]
-  pub fn get_fill_style(&self) -> napi::Result<String> {
+  pub fn get_fill_style(&self) -> Result<String> {
     let color = &self.canvas_fill_style;
     Ok(color.to_string())
   }
@@ -56,13 +68,13 @@ impl DrawContext {
   }
 
   #[napi(getter)]
-  pub fn get_stroke_style(&self) -> napi::Result<String> {
+  pub fn get_stroke_style(&self) -> Result<String> {
     let color = &self.canvas_stroke_style;
     Ok(color.to_string())
   }
 
   #[napi]
-  pub fn fill_rect(&mut self, x: i32, y: i32, width: i32, height: i32) -> napi::Result<()> {
+  pub fn fill_rect(&mut self, x: i32, y: i32, width: i32, height: i32) {
     let col = &self.canvas_fill_style;
     let color = utils::parse_color(col.to_string());
 
@@ -80,11 +92,10 @@ impl DrawContext {
       }),
       &DrawOptions::new(),
     );
-    Ok(())
   }
 
   #[napi]
-  pub fn stroke_rect(&mut self, x: i32, y: i32, width: i32, height: i32) -> napi::Result<()> {
+  pub fn stroke_rect(&mut self, x: i32, y: i32, width: i32, height: i32) {
     let col = &self.canvas_stroke_style;
     let color = utils::parse_color(col.to_string());
 
@@ -101,19 +112,47 @@ impl DrawContext {
         b: color.r,
         a: color.a,
       }),
-      &StrokeStyle {
-        cap: LineCap::Round,
-        join: LineJoin::Round,
-        width: 10.,
-        ..StrokeStyle::default()
-    },
+      &self.canvas_stroke_config,
       &DrawOptions::new(),
     );
-    Ok(())
+  }
+
+  #[napi(setter)]
+  pub fn set_line_width(&mut self, val: f64) {
+    self.canvas_stroke_config.width = val as f32;
+  }
+
+  #[napi(getter)]
+  pub fn get_line_width(&self) -> f64 {
+    self.canvas_stroke_config.width as f64
+  }
+
+  #[napi(js_name="__rq_surface_get_img_data_u8")]
+  pub fn get_image_data_buffer(&self) -> Buffer {
+    self.engine.get_data_u8().to_vec().into()
   }
 
   #[napi]
-  pub fn get_buffer(&self) -> napi::Result<Vec<u8>> {
-    Ok(self.engine.get_data_u8().to_vec())
+  pub fn set_line_dash(&mut self, dash_list: Vec<f64>) {
+    let len = dash_list.len();
+    let is_odd = len & 1 != 0;
+    let mut line_dash_list = if is_odd {
+      vec![0f32; len * 2]
+    } else {
+      vec![0f32; len]
+    };
+    for (idx, dash) in dash_list.iter().enumerate() {
+      line_dash_list[idx] = *dash as f32;
+      if is_odd {
+        line_dash_list[idx + len] = *dash as f32;
+      }
+    }
+
+    self.canvas_stroke_config.dash_array = line_dash_list;
+  }
+
+  #[napi]
+  pub fn get_line_dash(&self) -> Vec<f64> {
+    self.canvas_stroke_config.dash_array.iter().map(|i| *i as f64).collect()
   }
 }
